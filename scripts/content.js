9eea95cf7f60cbaf545da7e0f3c6502d5207b80d
Will Paceley <pace0033@algonquinlive.com>
2023-02-11 12:42:11 -0500

    refactor main audit function and start traversal code

--- scripts/content.js
+++ scripts/content.js
@@ -6,27 +6,49 @@ function init() {
 async function handleClick(ev) {
   ev.target.innerHTML = "Diagnosing";
 
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  console.log(tab.id);
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: getImages,
+    func: runAudit,
   });
 }
 
-function getImages() {
-  console.log("loaded");
-  document.querySelectorAll("img").forEach((img) => {
-    console.log(img);
-    if (img) {
-      var width = img.naturalWidth;
-      var height = img.naturalHeight;
+function runAudit() {
+  getImages();
+  traverseNodes();
 
-      console.log(
-        "The resolution of this image is " + width + " x " + height + " pixels."
-      );
+  // Functions called must be in local scope of runAudit()
+  function getImages() {
+    console.log("loaded");
+    document.querySelectorAll("img").forEach((img) => {
+      console.log(img);
+      if (img) {
+        var width = img.naturalWidth;
+        var height = img.naturalHeight;
+
+        console.log(
+          "The resolution of this image is " +
+            width +
+            " x " +
+            height +
+            " pixels."
+        );
+      }
+    });
+  }
+
+  function traverseNodes() {
+    console.log("inside traverseNodes");
+
+    for (const node in document.body.childNodes) {
+      console.log(node);
+
+      if (node.childNodes) {
+        traverseNodes(node.childNodes);
+      }
     }
-  });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", init);
