411d0a450f0abaa23ba7f92d475ddc9ab324ca08
JasreetK <90123505+JasreetK@users.noreply.github.com>
2023-02-11 11:56:06 -0500

    Inject function into current page

--- scripts/content.js
+++ scripts/content.js
@@ -3,8 +3,30 @@ function init() {
   button.addEventListener("click", handleClick);
 }
 
-function handleClick(ev) {
+async function handleClick(ev) {
   ev.target.innerHTML = "Diagnosing";
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  console.log(tab.id);
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: getImages,
+  });
+}
+
+function getImages() {
+  console.log("loaded");
+  document.querySelectorAll("img").forEach((img) => {
+    console.log(img);
+    if (img) {
+      var width = img.naturalWidth;
+      var height = img.naturalHeight;
+
+      console.log(
+        "The resolution of this image is " + width + " x " + height + " pixels."
+      );
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", init);
