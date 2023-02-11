2af39c93a16e195976a8e7406022646553d4a35e
Akshay Mahajan <maha0134@algonquinlive.com>
2023-02-11 12:06:54 -0500

    conflicts merged

--- scripts/content.js
+++ scripts/content.js
@@ -2,15 +2,6 @@ function init() {
   let button = document.getElementById("btn");
   button.addEventListener("click", handleClick);
 }
-function handleClick(ev) {
-  console.log("button clicked");
-  ev.target.textContent = "Diagnosing";
-  searchKeywords();
-}
-
-function searchKeywords() {
-  document.querySelectorAll("img").forEach((img) => console.log(img.src));
-}
 async function handleClick(ev) {
   ev.target.innerHTML = "Diagnosing";
 
@@ -35,7 +26,6 @@ function getImages() {
       );
     }
   });
->>>>>>> 411d0a450f0abaa23ba7f92d475ddc9ab324ca08
 }
 
 document.addEventListener("DOMContentLoaded", init);
